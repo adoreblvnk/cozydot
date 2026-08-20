4b8c5dbe1eed5da87ffc0ed3095b6d44ff1f7811
adoreblvnk <83258252+adoreblvnk@users.noreply.github.com>
2026-08-20 12:00:21 +0800

    refactor: simplify npm package handling

--- src/operations/packages/npm.rs
+++ src/operations/packages/npm.rs
@@ -8,9 +8,11 @@ pub(crate) fn install(host: &Host, packages: &[String]) -> Result<()> {
     };
     let mut missing = Vec::new();
     for package in packages {
-        let name = package_name(package);
-        let output =
-            host.output(&fnm, ["exec", "--using=default", "--", "npm", "list", "--global", "--depth=0", "--", name])?;
+        // get package name without the trailing version / tag
+        let name = package.rsplit_once('@').map_or(package.as_str(), |(name, _)| name);
+        let name = if name.is_empty() { package } else { name };
+        let args = ["exec", "--using=default", "--", "npm", "list", "--global", "--depth=0", "--", name];
+        let output = host.output(&fnm, args)?;
         if !output.status.success() {
             missing.push(package.clone());
         }
@@ -18,33 +20,22 @@ pub(crate) fn install(host: &Host, packages: &[String]) -> Result<()> {
     if missing.is_empty() {
         return Ok(());
     }
-    let mut npm_args = vec!["install".to_owned(), "--global".into(), "--".into()];
-    npm_args.extend(missing);
-    run_npm_checked(host, &fnm, "npm package install", npm_args)?;
+    let mut args = vec![
+        "exec".to_owned(),
+        "--using=default".into(),
+        "--".into(),
+        "npm".into(),
+        "install".into(),
+        "--global".into(),
+        "--".into(),
+    ];
+    args.extend(missing);
+    host.run("npm package install", &fnm, args)?;
     Ok(())
 }
 
 pub(crate) fn update(host: &Host) -> Result<()> {
     let Some(fnm) = fnm::find_executable(host)? else { return Ok(()) };
-    run_npm_checked(host, &fnm, "npm package update", ["update", "--global"])?;
+    host.run("npm package update", &fnm, ["exec", "--using=default", "--", "npm", "update", "--global"])?;
     Ok(())
-}
-
-fn package_name(package: &str) -> &str {
-    if package.starts_with('@') {
-        let slash = package.find('/').unwrap_or(package.len());
-        let version = package[slash..].find('@').map(|index| slash + index);
-        return version.map_or(package, |index| &package[..index]);
-    }
-    package.split_once('@').map_or(package, |(name, _)| name)
-}
-
-fn run_npm_checked<I, S>(host: &Host, fnm: &str, label: &str, npm_args: I) -> Result<std::process::Output>
-where
-    I: IntoIterator<Item = S>,
-    S: AsRef<str>,
-{
-    let mut args = vec!["exec".to_owned(), "--using=default".into(), "--".into(), "npm".into()];
-    args.extend(npm_args.into_iter().map(|arg| arg.as_ref().to_owned()));
-    host.run(label, fnm, args)
 }
